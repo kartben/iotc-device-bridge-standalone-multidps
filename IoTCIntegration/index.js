d63199f06d4faeff0423ddebe942cbdbfb42a25a
Benjamin Cabé <kartben@users.noreply.github.com>
2020-01-28 10:47:26 +0100

    First pass at implementing "multi-dps" support. Also switched to using azure-iot-provisioning-device npm package instead of manually implementing the DPS polling workflow over HTTP

--- IoTCIntegration/index.js
+++ IoTCIntegration/index.js
@@ -6,64 +6,31 @@
 const request = require('request-promise-native');
 const handleMessage = require('./lib/engine');
 
-const msiEndpoint = process.env.MSI_ENDPOINT;
-const msiSecret = process.env.MSI_SECRET;
+const express = require('express')
+const app = express()
 
-const parameters = {
-    idScope: process.env.ID_SCOPE,
-    primaryKeyUrl: process.env.IOTC_KEY_URL
+const DEVICE_ENROLLMENTS_MAP = {
+    "device010": {
+        idScope: '0ne0005CF6C',
+        primaryKey: 'Ox0GtLJ+JgKnPMQwoV0XplA0j/+8m7k6ZRK2CU+U2Q0EBSpCDkjaaO5SonjL9ANCPuYR33Qpx5aYN8xPg32EgQ=='
+     },    
+     "device020": {
+        idScope: '0ne0005CF6C',
+        primaryKey: 'Ox0GtLJ+JgKnPMQwoV0XplA0j/+8m7k6ZRK2CU+U2Q0EBSpCDkjaaO5SonjL9ANCPuYR33Qpx5aYN8xPg32EgQ=='
+     }
 };
 
-let kvToken;
-
-module.exports = async function (context, req) {
+app.use(express.json());
+app.post('/', async (req, res) => {
     try {
-        await handleMessage({ ...parameters, log: context.log, getSecret: getKeyVaultSecret }, req.body.device, req.body.measurements, req.body.timestamp);
+        await handleMessage({ deviceEnrollments: DEVICE_ENROLLMENTS_MAP, log: console.log }, req.body.device, req.body.measurements, req.body.timestamp);
+        res.status(200).end();
     } catch (e) {
-        context.log('[ERROR]', e.message);
-
-        context.res = {
-            status: e.statusCode ? e.statusCode : 500,
-            body: e.message
-        };
+        console.log('[ERROR]', e);
+        res.status(e.statusCode ? e.statusCode : 500).send(e.message);
     }
-}
-
-/**
- * Fetches a Key Vault secret. Attempts to refresh the token on authorization errors.
- */
-async function getKeyVaultSecret(context, secretUrl, forceTokenRefresh = false) {
-    if (!kvToken || forceTokenRefresh) {
-        const options = {
-            uri: `${msiEndpoint}/?resource=https://vault.azure.net&api-version=2017-09-01`,
-            headers: { 'Secret': msiSecret },
-            json: true
-        };
-
-        try {
-            context.log('[HTTP] Requesting new Key Vault token');
-            const response = await request(options);
-            kvToken = response.access_token;
-        } catch (e) {
-            throw new Error('Unable to get Key Vault token');
-        }
-    }
-
-    var options = {
-        url: `${secretUrl}?api-version=2016-10-01`,
-        headers : { 'Authorization' : `Bearer ${kvToken}` },
-        json: true
-    };
-
-    try {
-        context.log('[HTTP] Requesting Key Vault secret', secretUrl);
-        const response = await request(options);
-        return response && response.value;
-    } catch(e) {
-        if (e.statusCode === 401 && !forceTokenRefresh) {
-            return await getKeyVaultSecret(context, secretUrl, true);
-        } else {
-            throw new Error('Unable to fetch secret');
-        }
-    }
-}
+})
+  
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
--- IoTCIntegration/lib/engine.js
+++ IoTCIntegration/lib/engine.js
@@ -9,20 +9,19 @@ const Device = require('azure-iot-device');
 const DeviceTransport = require('azure-iot-device-http');
 const util = require('util');
 
+var ProvisioningTransport = require('azure-iot-provisioning-device-mqtt').Mqtt;
+var SymmetricKeySecurityClient = require('azure-iot-security-symmetric-key').SymmetricKeySecurityClient;
+var ProvisioningDeviceClient = require('azure-iot-provisioning-device').ProvisioningDeviceClient;
+
 const StatusError = require('../error').StatusError;
 
 const registrationHost = 'global.azure-devices-provisioning.net';
-const registrationSasTtl = 3600; // 1 hour
-const registrationApiVersion = `2018-11-01`;
-const registrationStatusQueryAttempts = 10;
-const registrationStatusQueryTimeout = 2000;
-const minDeviceRegistrationTimeout = 60*1000; // 1 minute
 
 const deviceCache = {};
 
 /**
  * Forwards external telemetry messages for IoT Central devices.
- * @param {{ idScope: string, primaryKeyUrl: string, log: Function, getSecret: (context: Object, secretUrl: string) => string }} context 
+ * @param {{ deviceEnrollments: Map, log: Function }} context 
  * @param {{ deviceId: string }} device 
  * @param {{ [field: string]: number }} measurements 
  */
@@ -84,87 +83,15 @@ async function getDeviceConnectionString(context, device) {
         return deviceCache[deviceId].connectionString;
     }
 
-    const connStr = `HostName=${await getDeviceHub(context, device)};DeviceId=${deviceId};SharedAccessKey=${await getDeviceKey(context, deviceId)}`;
+    var symmetricKey = await getDeviceKey(context, deviceId);
+    var provisioningSecurityClient = new SymmetricKeySecurityClient(deviceId, symmetricKey);
+    var provisioningClient = ProvisioningDeviceClient.create(registrationHost, context.deviceEnrollments[deviceId].idScope, new ProvisioningTransport(), provisioningSecurityClient);
+
+    var registrationResult = await provisioningClient.register();
+
+    const connStr = 'HostName=' + registrationResult.assignedHub + ';DeviceId=' + registrationResult.deviceId + ';SharedAccessKey=' + symmetricKey;
     deviceCache[deviceId].connectionString = connStr;
     return connStr;
-}
-
-/**
- * Registers this device with DPS, returning the IoT Hub assigned to it.
- */
-async function getDeviceHub(context, device) {
-    const deviceId = device.deviceId;
-    const now = Date.now();
-
-    // A 1 minute backoff is enforced for registration attempts, to prevent unauthorized devices
-    // from trying to re-register too often.
-    if (deviceCache[deviceId] && deviceCache[deviceId].lasRegisterAttempt && (now - deviceCache[deviceId].lasRegisterAttempt) < minDeviceRegistrationTimeout) {
-        const backoff = Math.floor((minDeviceRegistrationTimeout - (now - deviceCache[deviceId].lasRegisterAttempt)) / 1000);
-        throw new StatusError(`Unable to register device ${deviceId}. Minimum registration timeout not yet exceeded. Please try again in ${backoff} seconds`, 403);
-    }
-
-    deviceCache[deviceId] = {
-        ...deviceCache[deviceId],
-        lasRegisterAttempt: Date.now()
-    }
-
-    const sasToken = await getRegistrationSasToken(context, deviceId);
-
-    const registrationOptions = {
-        url: `https://${registrationHost}/${context.idScope}/registrations/${deviceId}/register?api-version=${registrationApiVersion}`,
-        method: 'PUT',
-        json: true,
-        headers: { Authorization: sasToken },
-        body: { registrationId: deviceId }
-    };
-
-    try {
-        context.log('[HTTP] Initiating device registration');
-        const response = await request(registrationOptions);
-
-        if (response.status !== 'assigning' || !response.operationId) {
-            throw new Error('Unknown server response');
-        }
-
-        const statusOptions = {
-            url: `https://${registrationHost}/${context.idScope}/registrations/${deviceId}/operations/${response.operationId}?api-version=${registrationApiVersion}`,
-            method: 'GET',
-            json: true,
-            headers: { Authorization: sasToken }
-        };
-
-        // The first registration call starts the process, we then query the registration status
-        // every 2 seconds, up to 10 times.
-        for (let i = 0; i < registrationStatusQueryAttempts; ++i) {
-            await new Promise(resolve => setTimeout(resolve, registrationStatusQueryTimeout));
-
-            context.log('[HTTP] Querying device registration status');
-            const statusResponse = await request(statusOptions);
-
-            if (statusResponse.status === 'assigning') {
-                continue;
-            } else if (statusResponse.status === 'assigned' && statusResponse.registrationState && statusResponse.registrationState.assignedHub) {
-                return statusResponse.registrationState.assignedHub;
-            } else if (statusResponse.status === 'failed' && statusResponse.registrationState && statusResponse.registrationState.errorCode === 400209) {
-                throw new StatusError('The device may be unassociated or blocked', 403);
-            } else {
-                throw new Error('Unknown server response');
-            }
-        }
-
-        throw new Error('Registration was not successful after maximum number of attempts');
-    } catch (e) {
-        throw new StatusError(`Unable to register device ${deviceId}: ${e.message}`, e.statusCode);
-    }
-}
-
-async function getRegistrationSasToken(context, deviceId) {
-    const uri = encodeURIComponent(`${context.idScope}/registrations/${deviceId}`);
-    const ttl = Math.round(Date.now() / 1000) + registrationSasTtl;
-    const signature = crypto.createHmac('sha256', new Buffer(await getDeviceKey(context, deviceId), 'base64'))
-        .update(`${uri}\n${ttl}`)
-        .digest('base64');
-    return`SharedAccessSignature sr=${uri}&sig=${encodeURIComponent(signature)}&skn=registration&se=${ttl}`;
 }
 
 /**
@@ -175,11 +102,14 @@ async function getDeviceKey(context, deviceId) {
         return deviceCache[deviceId].deviceKey;
     }
 
-    const key = crypto.createHmac('SHA256', Buffer.from(await context.getSecret(context, context.primaryKeyUrl), 'base64'))
-        .update(deviceId)
-        .digest()
-        .toString('base64');
+    const key = crypto.createHmac('SHA256', Buffer.from(context.deviceEnrollments[deviceId].primaryKey, 'base64'))
+        .update(deviceId, 'utf8')
+        .digest('base64');
 
-    deviceCache[deviceId].deviceKey = key;
+    deviceCache[deviceId] = {
+        ...deviceCache[deviceId],
+        deviceId: key
+    } 
+
     return key;
 }
